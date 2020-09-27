bcbd72514f7b786a9a23386c5711c665c7f69da3
Babar-Sajjad <babarsajjad@live.com>
2020-09-27 11:01:32 +0200

    comments added

--- txt_preprocessing.py
+++ txt_preprocessing.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 ## 1. Raw data processing
 
+# 
+
 # The 'household_power_consumption.txt' file has the following attributes:
 #    * Each data point has a date and time (hour) of recording
 #    * The data points are separated by semicolons (;)
